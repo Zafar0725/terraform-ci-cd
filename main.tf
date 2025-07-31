9a99505282498bd09a8a08b3838efe4c62be1520
Zafar0725 <Zshaik7671@conestogac.on.ca>
2025-07-31 15:15:11 -0400

    final changes

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "your-terraform-state-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
